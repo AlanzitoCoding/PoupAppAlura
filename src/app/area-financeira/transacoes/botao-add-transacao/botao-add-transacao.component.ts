5b23a830a2690ab68716f8fb71481fb7ee0ade37
AlanzitoCoding <alanzito006@gmail.com>
2025-11-04 10:51:16 -0300

    Patch Notes feat: criação do componente botao-add-transacao e inserção do mesmo no componente transacoes

---
+++ src/app/area-financeira/transacoes/botao-add-transacao/botao-add-transacao.component.ts
@@ -0,0 +1,13 @@
+// Louvado seja o Senhor
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-botao-add-transacao',
+  imports: [],
+  templateUrl: './botao-add-transacao.component.html',
+  styleUrl: './botao-add-transacao.component.css'
+})
+export class BotaoAddTransacaoComponent {
+
+}
--- src/app/area-financeira/transacoes/transacoes.component.ts
+++ src/app/area-financeira/transacoes/transacoes.component.ts
@@ -3,12 +3,12 @@
 import { Component, input } from '@angular/core';
 import { TransacaoComponent } from "./transacao/transacao.component";
 import { Transacao } from '../compartilhados/transacao.model';
-import { BotaoComponent } from "../../compartilhados/botao/botao.component";
 import { CardComponent } from '../compartilhados/card/card.component';
+import { BotaoAddTransacaoComponent } from './botao-add-transacao/botao-add-transacao.component';
 
 @Component({
   selector: 'app-transacoes',
-  imports: [TransacaoComponent, BotaoComponent, CardComponent],
+  imports: [TransacaoComponent, CardComponent, BotaoAddTransacaoComponent],
   templateUrl: './transacoes.component.html',
   styleUrl: './transacoes.component.css'
 })
